909780e72f874286b397b905f66dc069ec62c4ab
Ko8efanboy <ko8efanboy@gmail.com>
2024-05-11 17:50:20 -0700

    made loop and added bill

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,9 +10,12 @@
         string excuseJordan = "Jordan played plumbers and WE DONE WITH THE 90s. AND HE HAS NO LEFT";
         string goat = string.Empty;
         string keepGoing = "do you want to discuss anyone else?";
-        string excuseWilt = "Wilt was a great player, but he is not the GOAT.\nHe played against plumbers. \nHe was also 87 feet tall. \nWas the greatest athlete of all time though.";
-        string excuseKareem = "He got Passed by lebron in points \nPlayed in 70s which was the fastest paced and highest scoring era in NBA history. \nI will admit he did have the most unstoppable shot of all time.";
-        string excuseRussell = "Bill Russell was a civil rights legend but if we being real he played with 12 hall of famers at a time where there was like 8 teams in the leauge. \n11 Rings in 13 years is crazy though\nAlso never even average 19 points per game LMAO";
+        string excuseWilt =
+            "Wilt was a great player, but he is not the GOAT.\nHe played against plumbers. \nHe was also 87 feet tall. \nWas the greatest athlete of all time though.";
+        string excuseKareem =
+            "He got Passed by lebron in points \nPlayed in 70s which was the fastest paced and highest scoring era in NBA history. \nI will admit he did have the most unstoppable shot of all time.";
+        string excuseRussell =
+            "Bill Russell was a civil rights legend but if we being real he played with 12 hall of famers at a time where there was like 8 teams in the leauge. \n11 Rings in 13 years is crazy though\nAlso never even average 19 points per game LMAO";
 
         string lebronJames = "Lebron James";
         string kobeBryant = "Kobe Bryant";
@@ -21,7 +24,7 @@
         string kareemAbdul = "Kareem Abdul-Jabbar";
         string billRussell = "Bill Russell";
 
-My_label:
+        My_label:
         {
             Console.WriteLine(intro);
             Console.WriteLine(whoGoat);
@@ -31,56 +34,69 @@ My_label:
             {
                 goat = lebronJames;
             }
+
             if (response.Equals("jordan", StringComparison.OrdinalIgnoreCase))
             {
                 goat = michaelJordan;
             }
+
             if (response.Equals("kobe", StringComparison.OrdinalIgnoreCase))
             {
                 goat = kobeBryant;
             }
+
             if (response.Equals("wilt", StringComparison.OrdinalIgnoreCase))
             {
                 goat = wiltChamberlain;
             }
+
             if (response.Equals("i dont know", StringComparison.OrdinalIgnoreCase))
             {
                 goat = otherResponse;
             }
+
             if (response.Equals("kareem", StringComparison.OrdinalIgnoreCase))
             {
                 goat = kareemAbdul;
             }
+
             if (response.Equals("bill russell", StringComparison.OrdinalIgnoreCase))
             {
                 goat = billRussell;
             }
+
             if (goat == lebronJames)
             {
                 Console.WriteLine(excuseLebron);
             }
+
             if (goat == lebronJames)
             {
                 Console.WriteLine(excuseJordan);
             }
+
             if (goat == kobeBryant)
             {
                 Console.WriteLine(excuseKobe);
             }
+
             if (goat == wiltChamberlain)
             {
                 Console.WriteLine(excuseWilt);
             }
+
             if (goat == kareemAbdul)
             {
                 Console.WriteLine(excuseKareem);
             }
+
             if (goat == billRussell)
             {
                 Console.WriteLine(excuseRussell);
             }
+
             {
-                
+
             }
             Console.WriteLine(keepGoing);
             string continueAnswerStr = Console.ReadLine();
@@ -92,3 +108,4 @@ My_label:
         }
     }
 }
+    
